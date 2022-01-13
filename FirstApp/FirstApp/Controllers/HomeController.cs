7bac4480b8e35d24ee61290762b86d4e563926ea
Aygunaab <aygunaab@code.edu.az>
2022-01-13 11:44:58 -0500

    homework complete

---
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        public  ViewResult Index()
+        {
+          
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
